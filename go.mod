9f365838ee8fa5b63f956f0c794a0a25db85b85e
Torjas <Torben@Jastrow.info>
2022-09-07 21:40:00 +0200

    add logging

--- go.mod
+++ go.mod
@@ -12,4 +12,7 @@ require (
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/gson v0.7.1 // indirect
 	github.com/ysmood/leakless v0.8.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/carlescere/scheduler"
 	"github.com/go-rod/rod"
+	"go.uber.org/zap"
 	"net/http"
 	"os"
 	"runtime"
@@ -12,16 +12,22 @@ import (
 
 func main() {
 
+	test()
+	logger, _ := zap.NewProduction()
+
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	defer sugar.Errorln("Program exited")
 	browser := rod.New().MustConnect()
 
 	listings := make(map[string]bool)
 
 	job := func() {
-		println("check")
+		sugar.Info("New Check")
 		defer func() {
 			if r := recover(); r != nil {
+				sugar.Errorln("Recovered. Error:\n", r)
 
-				fmt.Println("Recovered. Error:\n", r)
 			}
 		}()
 
@@ -33,7 +39,6 @@ func main() {
 
 		elements := elist.MustElements("li")
 
-		fmt.Println("ELEMENTS")
 		newListings := make(map[string]string)
 		foundListings := make(map[string]bool)
 		for _, element := range elements {
@@ -49,19 +54,19 @@ func main() {
 
 				expandurl, err := ExpandUrl("https://inberlinwohnen.de" + *link.MustAttribute("href"))
 				if err != nil {
-					println("Error expanding url: " + err.Error())
+					sugar.Errorw("Error expanding url ", "error", err.Error(), "url", "https://inberlinwohnen.de"+*link.MustAttribute("href"))
+
 				} else {
 					newListings[headerText] = expandurl
 				}
 
 			}
 		}
-		//fmt.Println(el.MustText())
-		fmt.Println("END")
-
+		sugar.Infoln("Finished checking")
 		if n {
 			for s, s2 := range newListings {
-				fmt.Printf("%v: %v\n", s, s2)
+				sugar.Infow("New Listing", "name", s, "url", s2)
+
 			}
 			post(newListings)
 		}
@@ -83,6 +88,9 @@ func main() {
 }
 
 func post(newlistings map[string]string) {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
 	text := "Found new listings: \n\n"
 
 	for name, url := range newlistings {
@@ -96,18 +104,40 @@ func post(newlistings map[string]string) {
 	client := http.Client{}
 	req, err := http.NewRequest(http.MethodPost, os.Getenv("WATCHER_GCHAT_WEBHOOK"), &b)
 	if err != nil {
+		sugar.Errorw("Error creating request to chat", "error", err.Error(), "message_posted", message)
 		println("Error creating request: " + err.Error())
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		println("Error posting new listings: " + err.Error())
+		sugar.Errorw("Error posting new listings", "error", err.Error(), "message_posted", message)
+	}
+
+	defer resp.Body.Close()
+}
+
+func test() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, "https://wikipedia.org", nil)
+	if err != nil {
+		sugar.Errorw("Error creating test request to wikipedia", "error", err.Error())
+
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		sugar.Errorw("Error GETting wikipedia content", "error", err.Error())
+
 	}
 
 	defer resp.Body.Close()
 }
 
 func ExpandUrl(url string) (string, error) {
+
 	expandedUrl := url
 
 	client := &http.Client{
